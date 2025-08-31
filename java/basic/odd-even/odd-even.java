c4599b50eda0318de864f342459d96ee0a11182a
Luqman <luqmanchisty@gmail.com>
2025-08-31 16:05:13 +0530

    Update odd-even.java

--- java/basic/odd-even/odd-even.java
+++ java/basic/odd-even/odd-even.java
@@ -1,1 +1,15 @@
+import java.util.*;
 
+public class odd-even {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println(n + " is an even number.");
+        } else {
+            System.out.println(n + " is an odd number.");
+        }
+    }
+}
