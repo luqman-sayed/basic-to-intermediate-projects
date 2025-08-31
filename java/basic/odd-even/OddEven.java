eb058ce3cd24f0a113f5c49a2225a5195165e629
Luqman <luqmanchisty@gmail.com>
2025-08-31 16:16:26 +0530

    Update OddEven.java

--- java/basic/odd-even/OddEven.java
+++ java/basic/odd-even/OddEven.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class OddEven {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.println("Enter a number: ");
         int n = sc.nextInt();
 
         if (n % 2 == 0) {
